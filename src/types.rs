cf91340eec40cc0d42f06e5062bc714f056f05e2
SailorSnoW <snxwin@pm.me>
2022-08-13 01:03:35 +0200

    Implemented Bitwise enum Mods and using it for mods field of Replay

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,6 @@
 use crate::error::Error;
+use bitflags::bitflags;
+use core::default::Default;
 
 // Data types
 pub type Byte = u8;
@@ -47,40 +49,52 @@ impl TryFrom<Byte> for Gamemode {
     }
 }
 
-#[allow(dead_code)]
-pub enum Mod {
-    None = 0,
-    NoFail = 1,
-    Easy = 2,
-    TouchDevice = 4,
-    Hidden = 8,
-    HardRock = 16,
-    SuddenDeath = 32,
-    DoubleTime = 64,
-    Relax = 128,
-    HalfTime = 256,
-    Nightcore = 512, // Only set along with DoubleTime. i.e: NC only gives 576
-    Flashlight = 1024,
-    Autoplay = 2048,
-    SpunOut = 4096,
-    Relax2 = 8192,   // Autopilot
-    Perfect = 16384, // Only set along with SuddenDeath. i.e: PF only gives 16416
-    Key4 = 32768,
-    Key5 = 65536,
-    Key6 = 131072,
-    Key7 = 262144,
-    Key8 = 524288,
-    FadeIn = 1048576,
-    Random = 2097152,
-    Cinema = 4194304,
-    Target = 8388608,
-    Key9 = 16777216,
-    KeyCoop = 33554432,
-    Key1 = 67108864,
-    Key3 = 134217728,
-    Key2 = 268435456,
-    ScoreV2 = 536870912,
-    Mirror = 1073741824,
+bitflags! {
+    #[derive(Default)]
+    pub struct Mods: u32 {
+        const NONE           = 0;
+        const NO_FAIL         = 1;
+        const EASY           = 2;
+        const TOUCH_DEVICE    = 4;
+        const HIDDEN         = 8;
+        const HARDROCK       = 16;
+        const SUDDEN_DEATH    = 32;
+        const DOUBLETIME     = 64;
+        const RELAX          = 128;
+        const HALFTIME       = 256;
+        const NIGHTCORE      = 512; // Only set along with DoubleTime. i.e: NC only gives 576
+        const FLASHLIGHT     = 1024;
+        const AUTOPLAY       = 2048;
+        const SPUN_OUT        = 4096;
+        const RELAX2         = 8192;    // Autopilot
+        const PERFECT        = 16384; // Only set along with SuddenDeath. i.e: PF only gives 16416
+        const KEY4           = 32768;
+        const KEY55           = 65536;
+        const KEY6           = 13107;
+        const KEY7           = 262144;
+        const KEY8           = 524288;
+        const FADE_IN         = 1048576;
+        const RANDOM         = 2097152;
+        const CINEMA         = 4194304;
+        const TARGET         = 8388608;
+        const KEY0          = 16777216;
+        const KEY_COOP        = 33554432;
+        const KEY1           = 67108864;
+        const KEY3           = 134217728;
+        const KEY2           = 268435456;
+        const SCORE_V2        = 536870912;
+        const MIRROR         = 1073741824;
+    }
 }
 
-impl Mod {}
+impl From<Integer> for Mods {
+    fn from(value: Integer) -> Self {
+        Self::from_bits_truncate(value)
+    }
+}
+
+impl From<Mods> for Integer {
+    fn from(mods: Mods) -> Self {
+        mods.bits()
+    }
+}
